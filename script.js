a9d66615db35155bc3c40899edb8ce54b8ef7aee
Yan Shin <irrfan15@gmail.com>
2023-08-22 10:48:07 +0630

    Add completed task function

--- script.js
+++ script.js
@@ -6,15 +6,46 @@ window.onload = () => {
     
     
     addBtn.addEventListener("click", () =>{
+        //Adds task into list
         let userInput = document.querySelector("#task-input").value;
         taskList.push(userInput)
-        let newTask = document.createElement("div");
+        let newTask = document.createElement("div"); 
         newTask.innerText = userInput
         newTask.classList = "task-item"
-        for (let i = 0; i < taskList.length; i++){
-            taskOutput.append(newTask); 
+        
+
+        let doneBtn = document.createElement("input");
+        doneBtn.setAttribute('type', 'checkbox')
+        doneBtn.setAttribute('id', 'done-btn')
+
+        doneBtn.addEventListener("click", () =>{
+            newTask.classList.toggle("task-completed")
+        })
+
+        if(newTask.classList.contains("task-completed")){
+            newTask.style.backgroundColor = "red"
         }
-    })
+
+        
+
+        newTask.append(doneBtn)
+
+       
+
+        if(userInput == ""){
+           alert("Please add a task.")
+        }else{
+            for (let i = 0; i < taskList.length; i++){
+                taskOutput.append(newTask); 
+                
+                
+            }
+           
+        }
+        
+
+        
+    }
 
 
 
@@ -22,4 +53,4 @@ window.onload = () => {
 
 
 
-}
+)}
