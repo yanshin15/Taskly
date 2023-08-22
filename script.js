b4db1be7829ada54675a6a13591d26758f314f89
Yan Shin <irrfan15@gmail.com>
2023-08-22 09:18:53 +0630

    Add task feature

--- script.js
+++ script.js
@@ -2,11 +2,18 @@ window.onload = () => {
 
     let addBtn = document.getElementById("add-task-btn");
     let taskOutput = document.querySelector(".output-task-container")
+    let taskList = []
     
     
     addBtn.addEventListener("click", () =>{
         let userInput = document.querySelector("#task-input").value;
-        taskOutput.innerHTML = `<div>${userInput}</div>`
+        taskList.push(userInput)
+        let newTask = document.createElement("div");
+        newTask.innerText = userInput
+        newTask.classList = "task-item"
+        for (let i = 0; i < taskList.length; i++){
+            taskOutput.append(newTask); 
+        }
     })
 
 
